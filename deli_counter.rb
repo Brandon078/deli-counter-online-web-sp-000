911c4a05d174da17a693eb6c89a6243b7d6ca4de
brandon moro <brandon_078@hotmail.com>
2019-11-26 01:01:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(deli)
     puts "The line is currently empty."
   else
     current_line = "The line is currently:"
-    deli.each.with_index(1) do |person, number|
+    deli.each_with_index(1) do |person, number|
       current_line << "#{number}. #{person}"
     end
     puts current_line
@@ -18,7 +18,7 @@ end
 
 def now_serving(delo)
   if deli.empty?
-    puts "There is nobody waiting to be served!"
+    puts "The line is empty."
   else
     puts "Currently serving #{deli.first}."
     deli.shift
